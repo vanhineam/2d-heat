12d61a6a5a6ce0d53124bd5c6adad52c9d3b151f
Adam Van Hine <vanhine.adam@gmail.com>
2014-09-14 19:39:19 -0400

    openmp works

--- openmp.c
+++ openmp.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
-#include <time.h>
 
 void getInput(int*, int*);
 
 void main(int arc, char* argv) {
-    clock_t begin,end;
-    double time_spent;
+    omp_set_num_threads(4);
+    double begin,end;
+    double sequential_time, parallel_time;
     int iteration, i, j, current, next, N, T;
 
     getInput(&N, &T);
@@ -15,6 +15,16 @@ void main(int arc, char* argv) {
     //initaialize the array
     double h[2][N][N];
     double g[2][N][N];
+
+    // Initialize both arrays to all 0s
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            h[0][i][j] = 0;
+            h[1][i][j] = 0;
+            g[0][i][j] = 0;
+            g[1][i][j] = 0;
+        }
+    }
 
     // Set all the walls to 20C degrees
     for(i = 0; i < N; i++) {
@@ -40,38 +50,40 @@ void main(int arc, char* argv) {
     }
 
     // Get the start time of the program
-    begin = clock();
+    begin = omp_get_wtime();
 
-    // The actual calculation of the temperature of the room.
+    /**
+     * Begin the sequential calculation section
+     */
     current = 0;
     next = 1;
     for (iteration = 0; iteration < T; iteration++) {
         for( i = 1; i < N-1; i++) {
             for( j = 1; j < N-1; j++) {
-                h[next][i][j] = 0.25 * (h[current][i-1][j] + h[current][i+1][j]+ h[current][i][j-1] + h[current][i][j+1]);
+                h[next][i][j] = 0.25 * (h[current][i-1][j] + h[current][i+1][j] + h[current][i][j-1] + h[current][i][j+1]);
             }
         }
         current = next;
         next = (current + 1) % 2;
     }
-
-    // Get the end time
-    end = clock();
-    // Calculate the time spent
-    time_spent = (double)(end-begin) /CLOCKS_PER_SEC;
+    // Calculate time spent
+    end = omp_get_wtime();
+    sequential_time = end-begin;
 
     // Print an 8x8 array the N/8 x N/8 positions of the room.
     for(i = 0; i < N; i+= N/8) {
         for(j = 0; j < N; j+= N/8) {
-            printf("%.2f ", h[0][i][j]);
+            printf("%.2f ", h[current][i][j]);
         }
         printf("\n");
     }
     printf("\n");
 
 
-
-
+    /**
+     * Begin Parallel section
+     */
+    begin = omp_get_wtime();
     for (iteration = 0; iteration < T; iteration++) {
         #pragma omp parallel for private(i, j)
         for( i = 1; i < N-1; i++) {
@@ -82,6 +94,9 @@ void main(int arc, char* argv) {
         current = next;
         next = (current + 1) % 2;
     }
+    // Calculate the time spent
+    end = omp_get_wtime();
+    parallel_time = end - begin;
 
     // Print an 8x8 array the N/8 x N/8 positions of the room.
     for(i = 0; i < N; i+= N/8) {
@@ -92,16 +107,19 @@ void main(int arc, char* argv) {
     }
     printf("\n");
 
+    // Check to see if the two arrays match
     for(i = 0; i < N; i++) {
         for(j = 0; j < N; j++) {
-            if(g[0][i][j] != h[0][i][j]) {
+            if(g[0][i][j] != h[current][i][j]) {
                 printf("2 arrays do not match\n");
                 exit(1);
             }
         }
     }
+
     // Print the time in seconds that the program took to run
-    printf("The program took %f seconds to run.\n", time_spent);
+    printf("Parallel: %f\nSequential: %f\nDifference: %f\n", parallel_time, sequential_time, sequential_time - parallel_time);
+    printf("Speedup factor: %f\n", sequential_time/parallel_time);
 
 }
 
